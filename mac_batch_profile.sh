703d330ee2c2ec1040c8c19201d327538b6589c6
winFan <winfan@winFans-Mac-mini.local>
2014-09-29 16:44:03 +0800

    mv opt to ~/opt

--- mac_batch_profile.sh
+++ mac_batch_profile.sh
@@ -1,20 +1,20 @@
 JAVA_HOME=/Library/Java/Home
 JRE_HOME=$JAVA_HOME/jre
-ANDROID_HOME=/opt/sdk
-ANDROID_SDK_ROOT=/opt/sdk
+ANDROID_HOME=~/opt/sdk
+ANDROID_SDK_ROOT=~/opt/sdk
 
-ANDROID_NDK_ROOT=/opt/android-ndk-r9d
-NDK_ROOT=/opt/android-ndk-r9d
-ANDROID_NDK_HOME=/opt/android-ndk-r9d
+ANDROID_NDK_ROOT=~/opt/android-ndk-r9d
+NDK_ROOT=~/opt/android-ndk-r9d
+ANDROID_NDK_HOME=~/opt/android-ndk-r9d
 
-M2_HOME=/opt/maven
-QUICK_COCOS2DX_ROOT=/opt/quick-cocos2d-x-2.2.1-rc
-COCOS2DX_ROOT=/opt/quick-cocos2d-x-2.2.1-rc/lib/cocos2d-x
+M2_HOME=~/opt/maven
+QUICK_COCOS2DX_ROOT=~/opt/quick-cocos2d-x-2.2.1-rc
+COCOS2DX_ROOT=~/opt/quick-cocos2d-x-2.2.1-rc/lib/cocos2d-x
 EDITOR=vim
 CLIENT=/workplace/agent/SanGuo_Client_Agent
 TMP=/Volumes/Android原始包/Test/TMP
 
-COCOS_CONSOLE_ROOT=/opt/cocos2d-x-3.0/tools/cocos2d-console/bin
+COCOS_CONSOLE_ROOT=~/opt/cocos2d-x-3.0/tools/cocos2d-console/bin
 ANT_ROOT=/usr/local/bin
 
 PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$M2_HOME/bin:$COCOS_CONSOLE_ROOT
