b2ce83d28fb28bcf29eb95e723eb7e1d29f72833
winFan <winfan@winFans-Mac-mini.local>
2014-11-18 11:56:18 +0800

    add hosts

--- mac_batch_profile.sh
+++ mac_batch_profile.sh
@@ -10,6 +10,7 @@ ANDROID_NDK_HOME=~/opt/android-ndk-r9d
 M2_HOME=~/opt/maven
 QUICK_COCOS2DX_ROOT=~/opt/quick-cocos2d-x-2.2.1-rc
 COCOS2DX_ROOT=~/opt/quick-cocos2d-x-2.2.1-rc/lib/cocos2d-x
+COCOS2DX_HOME=/Users/winfan/workplace/cbzr
 EDITOR=vim
 CLIENT=/workplace/agent/SanGuo_Client_Agent
 TMP=/Volumes/Android原始包/Test/TMP
@@ -19,7 +20,7 @@ ANT_ROOT=/usr/local/bin
 
 PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$M2_HOME/bin:$COCOS_CONSOLE_ROOT
 CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar:$JRE_HOME/lib
-export COCOS_CONSOLE_ROOT ANT_ROOT NDK_ROOT JAVA_HOME JRE_HOME PATH CLASSPATH M2_HOME ANDROID_HOME SHARE COCOS2DX_ROOT ANDROID_SDK_ROOT ANDROID_NDK_ROOT  ANDROID_NDK_HOME VIM QUICK_COCOS2DX_ROOT EDITOR
+export COCOS_CONSOLE_ROOT ANT_ROOT NDK_ROOT JAVA_HOME JRE_HOME PATH CLASSPATH M2_HOME ANDROID_HOME SHARE COCOS2DX_ROOT ANDROID_SDK_ROOT ANDROID_NDK_ROOT  ANDROID_NDK_HOME VIM QUICK_COCOS2DX_ROOT EDITOR COCOS2DX_HOME
 
 export CLICOLOR=1
 
@@ -27,4 +28,15 @@ export CLICOLOR=1
 export PATH=/usr/local/bin:$PATH
 
 # Add environment variable COCOS_CONSOLE_ROOT for cocos2d-x
+
+
+##
+# Your previous /Users/winfan/.bash_profile file was backed up as /Users/winfan/.bash_profile.macports-saved_2014-10-09_at_10:45:58
+##
+
+# MacPorts Installer addition on 2014-10-09_at_10:45:58: adding an appropriate PATH variable for use with MacPorts.
+export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
+# Finished adapting your PATH environment variable for use with MacPorts.
+
+
 
